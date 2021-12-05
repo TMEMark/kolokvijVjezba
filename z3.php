d7af0ee3ea8f56a798abf72f5ca33ef4206a58a4
TMEMark <markobuljan.os@gmail.com>
2021-12-05 14:45:48 +0100

    Završeni svi zadaci

--- z3.php
+++ z3.php
@@ -1,12 +1,45 @@
+<?php
+    include "studiji.php";
+    include "studenti.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        *{
+            margin: 0px;
+            padding: 0px;
+            box-sizing: border-box;
+        }
+        body {
+            background-color: rgb(231, 231, 231);
+            font-family: 'Lato', sans-serif;
+        }
+    </style>
     <title>Zadatak 3</title>
 </head>
 <body>
-    
+    <h1>Tablica studija</h1>
+    <table border=1px >
+        <tr>
+            <th>Studiji</th>
+            <th>Studenti</th>
+        </tr>
+        <?php
+            foreach ($aktivni_studiji as $keyStudiji => $valueStudiji) {
+                $brojStudenata = 0;
+                foreach ($upisani_studenti as $keyStudenti => $valueStudenti) {
+                    if($valueStudenti["grupe"]["Grupa_prva"]==$keyStudiji || $valueStudenti["grupe"]["Grupa_druga"]==$keyStudiji){
+                        $brojStudenata++; 
+                    }
+                }
+
+                echo "<tr><td>". $valueStudiji["naziv_studija"] ."</td><td>". $brojStudenata ."</td></tr>";
+            }
+        ?>
+    </table>
 </body>
 </html>
